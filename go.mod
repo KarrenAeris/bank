c59747d6c8049111b4af74e15a36a707449c4df8
KarrenAeris <n.m.petrushkova@gmail.com>
2020-09-28 20:36:20 +0500

    v2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/KarrenAeris/bank/v1
+module github.com/KarrenAeris/bank/v2
 
 go 1.15
--- pkg/types/types.go
+++ pkg/types/types.go
@@ -2,31 +2,18 @@ package types
 
 type Money int64
 
-type Currency string
-
 type Category string
 
+type Status string
+
 const (
-	TJS Currency = "TJS"
-	RUB Currency = "RUB"
-	USD Currency = "USD"
+	StatusOk Status = "OK"
+	StatusFail Status = "FAIL"
+	StatusInProgress Status = "INPROGRESS"
 )
-
-type PAN string
-
-type Card struct {
-	ID int
-	PAN PAN
-	Balance Money
-	MinBalance Money
-	Currency Currency
-	Color string
-	Name string
-	Active bool
-}
-
 type Payment struct {
 	ID int
 	Amount Money
 	Category Category
+	Status Status
 }
